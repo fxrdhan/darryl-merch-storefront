0ba7aecd2a034d65ebebd5dc22774f6ecf404021
fxrdhan <firdausarief65@gmail.com>
2025-05-04 00:46:57 +0700

    fix(nav): enhance header styling for better visibility in dark mode

--- src/modules/layout/templates/nav/index.tsx
+++ src/modules/layout/templates/nav/index.tsx
@@ -15,7 +15,7 @@ export default async function Nav() {
 
   return (
     <div className="sticky top-0 inset-x-0 z-50 group">
-      <header className="relative h-16 mx-auto border-b duration-200 bg-white dark:bg-gray-900 border-ui-border-base dark:border-gray-700">
+      <header className="relative h-16 mx-auto border-b duration-200 border-ui-border-base dark:border-gray-700 bg-white/50 dark:bg-gray-900/50 backdrop-blur-lg">
         <nav className="content-container txt-xsmall-plus text-ui-fg-subtle flex items-center justify-between w-full h-full text-small-regular">
           <div className="flex-1 basis-0 h-full flex items-center">
             <div className="h-full">

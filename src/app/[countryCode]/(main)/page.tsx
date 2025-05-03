e5b985781777d26f22f3b9f0a0945b8dea25c1b8
fxrdhan <firdausarief65@gmail.com>
2025-05-03 23:57:50 +0700

    fix(page): adjust padding of the featured products section

--- src/app/[countryCode]/(main)/page.tsx
+++ src/app/[countryCode]/(main)/page.tsx
@@ -30,7 +30,7 @@ export default async function Home(props: {
   return (
     <>
       <Hero />
-      <div className="py-12">
+      <div className="py-0">
         <ul className="flex flex-col gap-x-6">
           <FeaturedProducts collections={collections} region={region} />
         </ul>

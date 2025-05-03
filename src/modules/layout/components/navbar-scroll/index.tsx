62e5344f5251f6d679d36894642c67702589bcd2
fxrdhan <firdausarief65@gmail.com>
2025-05-04 05:14:33 +0700

    feat(navbar-scroll): adjust margin for improved layout on larger screens

--- src/modules/layout/components/navbar-scroll/index.tsx
+++ src/modules/layout/components/navbar-scroll/index.tsx
@@ -42,7 +42,7 @@ const NavbarScroll = ({ children }: NavbarScrollProps) => {
             >
                 <div
                     className={`transition-all duration-300 ${isNavbarShrunk
-                        ? 'rounded-lg shadow-md mx-2 mt-2 overflow-hidden border border-ui-border-base dark:border-gray-700'
+                        ? 'rounded-lg shadow-md mx-2 mt-2 md:mx-60 overflow-hidden border border-ui-border-base dark:border-gray-700'
                         : 'border-b border-transparent' 
                     }`}
                 >

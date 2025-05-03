8038785bccf32af66ee38a6d6655cc79304ae2c4
fxrdhan <firdausarief65@gmail.com>
2025-05-04 01:06:30 +0700

    fix(layout): add suppressHydrationWarning to html tag

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -9,7 +9,7 @@ export const metadata: Metadata = {
 
 export default function RootLayout(props: { children: React.ReactNode }) {
   return (
-    <html lang="en">
+    <html lang="en" suppressHydrationWarning={true}>
       <body>
         <ThemeScript />
         <main className="relative">{props.children}</main>

65912f734c4ab643c77c9813bce919bc496c196a
fxrdhan <firdausarief65@gmail.com>
2025-05-04 02:55:45 +0700

    style(footer-country-select): format code

--- src/modules/layout/components/footer-country-select/index.tsx
+++ src/modules/layout/components/footer-country-select/index.tsx
@@ -7,21 +7,21 @@ import CountrySelect from "@modules/layout/components/country-select"
 import { ArrowRightMini } from "@medusajs/icons"
 
 export default function FooterCountrySelect({ regions }: { regions: HttpTypes.StoreRegion[] }) {
-  const toggleState = useToggleState()
+    const toggleState = useToggleState()
 
-  return (
-    <div
-      className="flex items-center gap-2"
-      onMouseEnter={toggleState.open}
-      onMouseLeave={toggleState.close}
-    >
-      <CountrySelect toggleState={toggleState} regions={regions} />
-      <ArrowRightMini
-        className={clx(
-          "transition-transform duration-300",
-          toggleState.state ? "-rotate-90" : ""
-        )}
-      />
-    </div>
-  )
+    return (
+        <div
+            className="flex items-center gap-2"
+            onMouseEnter={toggleState.open}
+            onMouseLeave={toggleState.close}
+        >
+            <CountrySelect toggleState={toggleState} regions={regions} />
+            <ArrowRightMini
+                className={clx(
+                    "transition-transform duration-300",
+                    toggleState.state ? "-rotate-90" : ""
+                )}
+            />
+        </div>
+    )
 }

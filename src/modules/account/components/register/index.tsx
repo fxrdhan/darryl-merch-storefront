cf1e2661543a0d36c6d0289a30757213e6662a50
fxrdhan <firdausarief65@gmail.com>
2025-05-03 10:55:21 +0700

    feat(register): update store name from "Medusa" to "Darryl" in registration agreement

--- src/modules/account/components/register/index.tsx
+++ src/modules/account/components/register/index.tsx
@@ -69,7 +69,7 @@ const Register = ({ setCurrentView }: Props) => {
         </div>
         <ErrorMessage error={message} data-testid="register-error" />
         <span className="text-center text-ui-fg-base text-small-regular mt-6">
-          By creating an account, you agree to Medusa Store&apos;s{" "}
+          By creating an account, you agree to Darryl Store&apos;s{" "}
           <LocalizedClientLink
             href="/content/privacy-policy"
             className="underline"

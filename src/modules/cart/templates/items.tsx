811d6898e4c1d4ef818a837e4f1c37d61bcc2dcc
fxrdhan <firdausarief65@gmail.com>
2025-05-04 12:35:38 +0700

    fix(items-template): enhance dark mode support in items template styles

--- src/modules/cart/templates/items.tsx
+++ src/modules/cart/templates/items.tsx
@@ -16,16 +16,16 @@ const ItemsTemplate = ({ cart }: ItemsTemplateProps) => {
       <div className="pb-3 flex items-center dark:text-white">
         <Heading className="text-[2rem] leading-[2.75rem]">Cart</Heading>
       </div>
-      <Table className="dark:text-gray-300">
+      <Table className="dark:text-gray-300 dark:bg-gray-700">
         <Table.Header className="border-t-0">
-          <Table.Row className="text-ui-fg-subtle txt-medium-plus">
-            <Table.HeaderCell className="!pl-0">Item</Table.HeaderCell>
+          <Table.Row className="text-ui-fg-subtle txt-medium-plus dark:bg-gray-900">
+            <Table.HeaderCell className="!pl-0 text-center">Item</Table.HeaderCell>
             <Table.HeaderCell></Table.HeaderCell>
             <Table.HeaderCell>Quantity</Table.HeaderCell>
             <Table.HeaderCell className="hidden small:table-cell">
               Price
             </Table.HeaderCell>
-            <Table.HeaderCell className="!pr-0 text-right">
+            <Table.HeaderCell className="!pr-0 text-center">
               Total
             </Table.HeaderCell>
           </Table.Row>

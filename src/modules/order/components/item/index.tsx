67d468185a7c046281089f675d63bf00ddf78d67
fxrdhan <firdausarief65@gmail.com>
2025-05-04 14:31:51 +0700

    fix(item): enhance dark mode support in table row background

--- src/modules/order/components/item/index.tsx
+++ src/modules/order/components/item/index.tsx
@@ -13,7 +13,7 @@ type ItemProps = {
 
 const Item = ({ item, currencyCode }: ItemProps) => {
   return (
-    <Table.Row className="w-full" data-testid="product-row">
+    <Table.Row className="w-full dark:bg-gray-800" data-testid="product-row">
       <Table.Cell className="!pl-0 p-4 w-24">
         <div className="flex w-16">
           <Thumbnail thumbnail={item.thumbnail} size="square" />

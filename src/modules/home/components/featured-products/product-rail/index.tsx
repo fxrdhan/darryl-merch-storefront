8e83150f1aa8a4eff67079e12430e218e2427e0a
fxrdhan <firdausarief65@gmail.com>
2025-05-04 10:33:51 +0700

    feat(product-rail): refactor product rail component to include cheapest price in product mapping

--- src/modules/home/components/featured-products/product-rail/index.tsx
+++ src/modules/home/components/featured-products/product-rail/index.tsx
@@ -1,5 +1,6 @@
 import { listProducts } from "@lib/data/products"
 import { FindParams, HttpTypes, StoreProductParams } from "@medusajs/types"
+import { getProductPrice } from "@lib/util/get-product-price"
 import { Text } from "@medusajs/ui"
 
 import InteractiveLink from "@modules/common/components/interactive-link"
@@ -13,7 +14,7 @@ export default async function ProductRail({
   region: HttpTypes.StoreRegion
 }) {
   const {
-    response: { products: pricedProducts },
+    response: { products },
   } = await listProducts({
     regionId: region.id,
     queryParams: {
@@ -22,9 +23,14 @@ export default async function ProductRail({
     } as FindParams & StoreProductParams & { collection_id?: string[] },
   })
 
-  if (!pricedProducts) {
+  if (!products || products.length === 0) {
     return null
   }
+
+  const productsWithPrice = products.map(product => {
+    const { cheapestPrice } = getProductPrice({ product });
+    return { ...product, cheapestPrice };
+  })
 
   return (
     <div className="content-container py-12 small:py-24">
@@ -34,11 +40,15 @@ export default async function ProductRail({
           View all
         </InteractiveLink>
       </div>
-      <ul className="grid grid-cols-2 small:grid-cols-3 gap-x-6 gap-y-24 small:gap-y-36">
-        {pricedProducts &&
-          pricedProducts.map((product) => (
+      <ul className="grid grid-cols-2 small:grid-cols-3 medium:grid-cols-4 gap-x-6 gap-y-8">
+        {productsWithPrice &&
+          productsWithPrice.map((product) => (
             <li key={product.id}>
-              <ProductPreview product={product} region={region} isFeatured />
+              <ProductPreview
+                product={product}
+                region={region}
+                isFeatured
+              />
             </li>
           ))}
       </ul>

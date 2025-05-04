a81c2e61b181a2fbd7c9c4203ff5a5af4e1c1911
fxrdhan <firdausarief65@gmail.com>
2025-05-04 11:36:38 +0700

    fix(sign-in-prompt): enhance dark mode support in sign-in prompt component styles

--- src/modules/cart/components/sign-in-prompt/index.tsx
+++ src/modules/cart/components/sign-in-prompt/index.tsx
@@ -3,12 +3,12 @@ import LocalizedClientLink from "@modules/common/components/localized-client-lin
 
 const SignInPrompt = () => {
   return (
-    <div className="bg-white flex items-center justify-between">
+    <div className="bg-white dark:bg-gray-800 flex items-center justify-between">
       <div>
-        <Heading level="h2" className="txt-xlarge">
+        <Heading level="h2" className="txt-xlarge dark:text-white">
           Already have an account?
         </Heading>
-        <Text className="txt-medium text-ui-fg-subtle mt-2">
+        <Text className="txt-medium text-ui-fg-subtle dark:text-gray-400 mt-2">
           Sign in for a better experience.
         </Text>
       </div>

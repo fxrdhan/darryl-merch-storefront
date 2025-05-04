948dac43818bb4e76a203379573538adfe6b788c
fxrdhan <firdausarief65@gmail.com>
2025-05-04 14:31:29 +0700

    fix(checkout-summary): enhance dark mode support in background styles

--- src/modules/checkout/templates/checkout-summary/index.tsx
+++ src/modules/checkout/templates/checkout-summary/index.tsx
@@ -7,8 +7,8 @@ import Divider from "@modules/common/components/divider"
 
 const CheckoutSummary = ({ cart }: { cart: any }) => {
   return (
-    <div className="sticky top-0 flex flex-col-reverse small:flex-col gap-y-8 py-8 small:py-0 ">
-      <div className="w-full bg-white flex flex-col">
+    <div className="sticky top-16 flex flex-col-reverse small:flex-col gap-y-8 py-8 small:py-0 ">
+      <div className="w-full bg-white dark:bg-gray-800 shadow-sm rounded-lg p-6 flex flex-col">
         <Divider className="my-6 small:hidden" />
         <Heading
           level="h2"

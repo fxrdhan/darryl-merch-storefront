d0c16bd708d46d82deb6ba82b6387c2cbcba065e
fxrdhan <firdausarief65@gmail.com>
2025-05-04 10:56:20 +0700

    fix(layout): ensure navbar brand link inactive state respects theme

--- src/modules/layout/templates/nav/index.tsx
+++ src/modules/layout/templates/nav/index.tsx
@@ -37,7 +37,7 @@ export default async function Nav() {
             <div className="flex items-center h-full">
               <LocalizedClientLink
                 href="/"
-                className="txt-compact-large-plus md:txt-compact-xlarge-plus hover:text-ui-fg-base dark:text-white dark:hover:text-gray-500 uppercase"
+                className="txt-compact-large-plus md:txt-compact-xlarge-plus text-ui-fg-subtle hover:text-ui-fg-base dark:text-gray-200 dark:hover:text-gray-500 uppercase"
                 data-testid="nav-store-link"
               >
                 Darryl Store

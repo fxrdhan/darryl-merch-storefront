0ca492320bf0a00efba3a2150d9ebcee15ecdd0a
fxrdhan <firdausarief65@gmail.com>
2025-05-04 09:54:35 +0700

    feat(featured-products): enhance product rendering logic to display products or collections based on availability

--- src/modules/home/components/featured-products/index.tsx
+++ src/modules/home/components/featured-products/index.tsx
@@ -1,16 +1,39 @@
 import { HttpTypes } from "@medusajs/types"
 import ProductRail from "@modules/home/components/featured-products/product-rail"
+import ProductPreview from "@modules/products/components/product-preview"
 
 export default async function FeaturedProducts({
   collections,
+  products,
   region,
 }: {
-  collections: HttpTypes.StoreCollection[]
+  collections?: HttpTypes.StoreCollection[]
+  products?: HttpTypes.StoreProduct[]
   region: HttpTypes.StoreRegion
 }) {
-  return collections.map((collection) => (
-    <li key={collection.id}>
-      <ProductRail collection={collection} region={region} />
-    </li>
-  ))
+  if (products) {
+    return (
+      <div className="content-container py-12 small:py-24">
+        <h2 className="text-2xl-semi mb-8">Check out our products</h2>
+        <ul className="grid grid-cols-2 small:grid-cols-3 medium:grid-cols-4 gap-x-6 gap-y-8">
+          {products.map((product) => (
+            <li key={product.id}>
+              <ProductPreview product={product} region={region} />
+            </li>
+          ))}
+        </ul>
+      </div>
+    )
+  } else if (collections) {
+    return (
+      <ul className="flex flex-col gap-y-6">
+        {collections.map((collection) => (
+          <li key={collection.id}>
+            <ProductRail collection={collection} region={region} />
+          </li>
+        ))}
+      </ul>
+    )
+  }
+  return null
 }

3b2a21a1446ea69962c6fb66a5d5be1bcf2b8199
fxrdhan <firdausarief65@gmail.com>
2025-05-04 14:31:20 +0700

    fix(payment-container): enhance dark mode support in styles and text colors

--- src/modules/checkout/components/payment-container/index.tsx
+++ src/modules/checkout/components/payment-container/index.tsx
@@ -34,7 +34,7 @@ const PaymentContainer: React.FC<PaymentContainerProps> = ({
       value={paymentProviderId}
       disabled={disabled}
       className={clx(
-        "flex flex-col gap-y-2 text-small-regular cursor-pointer py-4 border rounded-rounded px-8 mb-2 hover:shadow-borders-interactive-with-active",
+        "flex flex-col gap-y-2 text-small-regular cursor-pointer py-4 border dark:border-gray-700 rounded-rounded px-8 mb-2 hover:shadow-borders-interactive-with-active dark:hover:border-gray-500",
         {
           "border-ui-border-interactive":
             selectedPaymentOptionId === paymentProviderId,
@@ -44,7 +44,7 @@ const PaymentContainer: React.FC<PaymentContainerProps> = ({
       <div className="flex items-center justify-between ">
         <div className="flex items-center gap-x-4">
           <Radio checked={selectedPaymentOptionId === paymentProviderId} />
-          <Text className="text-base-regular">
+          <Text className="text-base-regular dark:text-gray-300">
             {paymentInfoMap[paymentProviderId]?.title || paymentProviderId}
           </Text>
           {isManual(paymentProviderId) && isDevelopment && (
@@ -85,7 +85,7 @@ export const StripeCardContainer = ({
       style: {
         base: {
           fontFamily: "Inter, sans-serif",
-          color: "#424270",
+          color: "rgb(107 114 128 / var(--tw-text-opacity, 1))", // Adjusted to match dark:text-gray-400 approx
           "::placeholder": {
             color: "rgb(107 114 128)",
           },
@@ -93,7 +93,7 @@ export const StripeCardContainer = ({
       },
       classes: {
         base: "pt-3 pb-1 block w-full h-11 px-4 mt-0 bg-ui-bg-field border rounded-md appearance-none focus:outline-none focus:ring-0 focus:shadow-borders-interactive-with-active border-ui-border-base hover:bg-ui-bg-field-hover transition-all duration-300 ease-in-out",
-      },
+      }, // Ensure these classes or UI tokens handle dark mode
     }
   }, [])
 

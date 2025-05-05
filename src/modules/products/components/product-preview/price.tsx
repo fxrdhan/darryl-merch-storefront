3d57fd87cf69aca1b2a30e72c5aa152a36171796
fxrdhan <firdausarief65@gmail.com>
2025-05-05 19:47:40 +0700

    fix(price): enhance text styling for original and calculated prices in PreviewPrice component

--- src/modules/products/components/product-preview/price.tsx
+++ src/modules/products/components/product-preview/price.tsx
@@ -10,14 +10,14 @@ export default function PreviewPrice({ price }: { price: VariantPrice }) {
     <>
       {price.price_type === "sale" && (
         <Text
-          className="line-through text-ui-fg-muted"
+          className="line-through text-ui-fg-muted text-xs md:text-sm"
           data-testid="original-price"
         >
           {price.original_price}
         </Text>
       )}
       <Text
-        className={clx("text-ui-fg-muted", {
+        className={clx("text-ui-fg-muted text-xs md:text-sm", {
           "text-ui-fg-interactive": price.price_type === "sale",
         })}
         data-testid="price"

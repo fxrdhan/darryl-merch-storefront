33fd67698cb776ade6e89fa8601da2b5b07336ed
fxrdhan <firdausarief65@gmail.com>
2025-05-05 12:55:39 +0700

    style(medusa-cta): add responsive text size for improved readability

--- src/modules/layout/components/medusa-cta/index.tsx
+++ src/modules/layout/components/medusa-cta/index.tsx
@@ -10,7 +10,7 @@ import TypeScript from "../../../common/icons/typescript"
 
 const MedusaCTA = () => {
   return (
-    <Text className="flex gap-x-2 txt-compact-small-plus items-center">
+    <Text className="flex gap-x-2 txt-compact-small-plus md:text-base items-center">
       Built with
       <a href="https://www.medusajs.com" target="_blank" rel="noreferrer">
         <Medusa fill="#9ca3af" className="fill-[#9ca3af]" />

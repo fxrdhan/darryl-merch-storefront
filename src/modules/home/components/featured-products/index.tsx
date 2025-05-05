0fe3ec08e75a9ae92d9ec1403099cccb0b517d86
fxrdhan <firdausarief65@gmail.com>
2025-05-05 20:11:44 +0700

    fix(featured-products): update layout for FeaturedProducts component to enhance styling consistency

--- src/modules/home/components/featured-products/index.tsx
+++ src/modules/home/components/featured-products/index.tsx
@@ -22,7 +22,7 @@ export default async function FeaturedProducts({
     });
 
     return (
-      <div className="py-8 md:py-12">
+      <div className="relative">
         <ProductCarousel products={productsWithPrice} region={region} />
       </div>
     );
@@ -34,7 +34,7 @@ export default async function FeaturedProducts({
     }
 
     return (
-      <div className="py-0 md:py-12">
+      <div className="relative">
         <div className="content-container">
           <h2 className="text-2xl-regular mb-8 lg:text-4xl text-center">{firstCollectionWithProducts.title}</h2>
         </div>

299ccfa448ea63640accdbc4781d69225486a0b0
fxrdhan <firdausarief65@gmail.com>
2025-05-09 10:05:00 +0700

    feat(image-gallery): add motion effects to image transitions in gallery

--- src/modules/products/components/image-gallery/index.tsx
+++ src/modules/products/components/image-gallery/index.tsx
@@ -1,6 +1,9 @@
+"use client";
+
 import { HttpTypes } from "@medusajs/types"
 import { Container } from "@medusajs/ui"
 import Image from "next/image"
+import { motion } from "framer-motion"
 
 type ImageGalleryProps = {
   images: HttpTypes.StoreProductImage[]
@@ -18,17 +21,24 @@ const ImageGallery = ({ images }: ImageGalleryProps) => {
               id={image.id}
             >
               {!!image.url && (
-                <Image
-                  src={image.url}
-                  priority={index <= 2 ? true : false}
-                  className="absolute inset-0 rounded-rounded"
-                  alt={`Product image ${index + 1}`}
-                  fill
-                  sizes="(max-width: 576px) 280px, (max-width: 768px) 360px, (max-width: 992px) 480px, 800px"
-                  style={{
-                    objectFit: "cover",
-                  }}
-                />
+                <motion.div
+                  initial={{ scale: 1.1, filter: "blur(10px)", opacity: 0 }}
+                  animate={{ scale: 1, filter: "blur(0px)", opacity: 1 }}
+                  transition={{ duration: 0.5, delay: index * 0.1 }}
+                  className="absolute inset-0"
+                >
+                  <Image
+                    src={image.url}
+                    priority={index <= 2 ? true : false}
+                    className="absolute inset-0 rounded-rounded"
+                    alt={`Product image ${index + 1}`}
+                    fill
+                    sizes="(max-width: 576px) 280px, (max-width: 768px) 360px, (max-width: 992px) 480px, 800px"
+                    style={{
+                      objectFit: "cover",
+                    }}
+                  />
+                </motion.div>
               )}
             </Container>
           )
